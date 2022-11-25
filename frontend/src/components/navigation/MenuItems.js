2fef72fbc2edf60f8c167ed0b623d6ed62300a19
Kris Vlaskamp <56298946+ctrl-alt-kris@users.noreply.github.com>
2022-11-25 15:48:12 +0100

    nav working

--- frontend/src/components/navigation/MenuItems.js
+++ frontend/src/components/navigation/MenuItems.js
@@ -1,19 +1,27 @@
 import { UserAuth } from '../context/AuthContext';
 import MenuItem from './MenuItem';
 import StraightIcon from '@mui/icons-material/Straight';
-import { isPropertySignature } from 'typescript';
+import HomeIcon from '@mui/icons-material/Home';
 
 const MenuItems = (props) => {
   const { userInfo } = UserAuth();
 
   if (userInfo.role === 'user') {
     return (
-      <MenuItem
-        expanded={props.expanded}
-        icon={<StraightIcon fontSize="large" />}
-        url={'/path'}
-        title={'My path'}
-      />
+      <>
+        <MenuItem
+          expanded={props.expanded}
+          icon={<HomeIcon fontSize="large" />}
+          url={'/'}
+          title={'Home'}
+        />
+        <MenuItem
+          expanded={props.expanded}
+          icon={<StraightIcon fontSize="large" />}
+          url={'/path'}
+          title={'My path'}
+        />
+      </>
     );
   } else if (userInfo === 'creator') {
     return <div>creator</div>;
